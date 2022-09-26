15871966b28df96464e690fa2c281192a085a232
chazzlee <chazz@dev.com>
2022-09-26 16:52:06 -0400

    add more methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,29 +1,34 @@
 # frozen_string_literal: true
 
 class Array
-  # hey do you see this?
-
   def my_each(&block)
-
     i = 0
-    while i < self.size
+    while i < size
       block.call(self[i])
       i += 1
     end
-
     self
-
   end
 
   def my_select(&block)
-
     result = []
-
-    self.my_each do |ele|
-      if block.call(ele)
-        result.push(ele)
-      end
-    end
+    my_each { |ele| result.push(ele) if block.call(ele) }
     result
+  end
+
+  def my_reject(&block)
+    result = []
+    my_each { |ele| result.push(ele) unless block.call(ele) }
+    result
+  end
+
+  def my_any?(&block)
+    my_each { |el| return true if block.call(el) }
+    false
+  end
+
+  def my_all?(&block)
+    my_each { |el| return false unless block.call(el) }
+    true
   end
 end
