7cedbf5f299ac8c7fb42be6482c5a4eb5ea672f4
Arnob <arnobdam@gmail.com>
2022-09-26 16:28:21 -0400

    Chazz's amazing enumerables

--- enumerables.rb
+++ enumerables.rb
@@ -2,4 +2,28 @@
 
 class Array
   # hey do you see this?
+
+  def my_each(&block)
+
+    i = 0
+    while i < self.size
+      block.call(self[i])
+      i += 1
+    end
+
+    self
+
+  end
+
+  def my_select(&block)
+
+    result = []
+
+    self.my_each do |ele|
+      if block.call(ele)
+        result.push(ele)
+      end
+    end
+    result
+  end
 end
